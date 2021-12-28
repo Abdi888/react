34a46f15e5cc66658521c11d8493440a906c1ac0
samiir bile <samiirbile69@gmail.com>
2021-12-28 19:54:42 +0300

    new added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='main'>
+     <div className='navbar'>
+       <div className='icon'>
+         <h2 className='logo'>praros</h2>
+       </div>
+
+       <div className='menu'>
+         <ul>
+           <li><a href='#'>HOME</a></li>
+           <li><a href='#'>ABOUT</a></li>
+           <li><a href='#'>SERVICE</a></li>
+           <li><a href='#'>DESIGN</a></li>
+           <li><a href='#'>CONTACT</a></li>
+         </ul>
+       </div>
+
+       <div className='search'>
+         <input className='srch' type="search"  name='' placeholder='type to text'/>
+         <a href='#'><button className="btn">search</button></a>
+       </div>
+
+       <div className='content'>
+         <h1>web design & <br /><span>Development</span><br /></h1>
+         <p className='par'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          <br />Saepe magnam quo quisquam rem aspernatur distinctio nihil, tempore ab, <br />esse ratione corporis iure ex accusantium rerum, nobis sequi eaque quae eligendi!</p>
+          <button className="cn"><a href='#'>JOIN AS</a></button>
+       </div>
+     </div>
+   </div>
   );
 }
 
---
+++ src/components/Components.jsx
@@ -0,0 +1,6 @@
+const name=() =>{
+     return (
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt aspernatur ut iusto, unde placeat dolorem id? Recusandae expedita dolorem minus iusto doloribus obcaecati. Magni, atque dolorum. Eos perferendis sit totam!</p>
+     )
+}
+export default name;
